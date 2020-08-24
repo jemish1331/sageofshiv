c552b7381d7ca72b09be60abc9d73ba7fec3f01d
jemish italiya <italiyajemish99@gmail.com>
2020-08-24 19:31:19 +0530

    first commit

--- src/MainPage.js
+++ src/MainPage.js
@@ -11,8 +11,6 @@ import jemish from "./images/jemish.jpg";
 import InstagramIcon from '@material-ui/icons/Instagram';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import CallIcon from '@material-ui/icons/Call';
-import { Collapse } from "@material-ui/core";
-import  {Media} from "reactstrap";
 
 
 function ncard1(val)
@@ -28,33 +26,32 @@ data={val.data}
 
 const MainPage=()=>{
     let year=new Date().getFullYear();
-    let prop="btn d-inline btn-primary ";
     
     return(
-        <>
         
-        <BrowserRouter>
-    <Media className="container">
-        <NavLink to="/home" activeClassName="selected" replace><button className={prop}>Profile</button></NavLink>
-        <NavLink to="/shoulder" activeClassName="selected" ><button className={prop}>Shoulder-Exercise</button></NavLink> 
-        <NavLink to="/chest" activeClassName="selected" ><button className={prop}>Chest-Exercise</button></NavLink>
-        <NavLink to="/leg" activeClassName="selected" ><button className={prop}>Leg-Exercise</button></NavLink>
-        <NavLink to="/bicep" activeClassName="selected" ><button className={prop}>Bicep-Exercise</button></NavLink>
-        <NavLink to="/timer" activeClassName="selected" ><button className={prop}>Stopwatch && Timer</button></NavLink>
-        </Media>
-        
+        <React.Fragment>
+       <BrowserRouter>
+       <div className="fixed-top mt-4 gray small-h-25 d-flex">
+        <NavLink to="/home" activeClassName="selected" className="ml-4 text-primary  h4 text-success">Profile</NavLink>
+        <NavLink to="/shoulder" activeClassName="selected"  className="ml-4 text-primary h4 text-success">Shoulder-Exercise</NavLink> 
+        <NavLink to="/chest" activeClassName="selected" className="ml-4 text-primary h4 text-success ">Chest-Exercise</NavLink>
+        <NavLink to="/leg" activeClassName="selected" className="ml-4 text-primary h4 text-success">Leg-Exercise</NavLink>
+        <NavLink to="/bicep" activeClassName="selected" className="ml-4 text-primary h4 text-success" >Bicep-Exercise</NavLink>
+        <NavLink to="/timer" activeClassName="selected" className="ml-4 text-primary h4 text-success">Diet-plan</NavLink>
+       </div>
+       
+       
         <Route exact path="/" render={()=>{
             return (
                 <>
                 <br />
                 <br />
-            
-                <img src="https://i.pinimg.com/736x/67/50/8c/67508cacd7406a4cb80ee7bf489a27f9.jpg" className="ml-5 w-50 h-40 mt-50px "></img>
+                <br />
+                <br/>
+                <img src="https://i.pinimg.com/736x/67/50/8c/67508cacd7406a4cb80ee7bf489a27f9.jpg" className="ml-5 w-50 h-40 "></img>
                  <br/>
-                 <h1 className="ml-4 "><Collapse>To be continued....</Collapse> </h1>
-                
-                
-                 </>
+                 <h1 className="ml-4 ">To be continued....</h1>
+                </>
             );
 
         }} />
@@ -64,7 +61,8 @@ const MainPage=()=>{
                 <>
                 <br/>
                 <br/>
-                <img src={jemish} className="ml-5 w-50 h-50"></img><br/>
+                <br/>
+                <img src={jemish} className="ml-5 w-50 mt-5 h-50"></img><br/>
                 <br/>
                <a href="https://instagram.com/jemish_italiya_?igshid=i1nrk81esd3j"> <InstagramIcon className="insta-whatsapp" /></a>
                 <a href="mailto:italiyajemish99@gmail.com"><DraftsIcon className="insta-whatsapp" /></a>
@@ -79,7 +77,7 @@ const MainPage=()=>{
         <Route exact path="/shoulder" render={()=>{
             return(
             <>
-            <br/>
+           <br />
            {Shdata.map(ncard1)}
            </>
             );
@@ -88,7 +86,7 @@ const MainPage=()=>{
         <Route path="/Chest" render={()=>{
             return(
             <>
-            <br/>
+         <br />
            {Cedata.map(ncard1)}
            </>
             );
@@ -96,7 +94,7 @@ const MainPage=()=>{
         <Route path="/Leg" render={()=>{
             return(
             <>
-            <br/>
+           <br />
            {Ledata.map(ncard1)}
            </>
             );
@@ -104,24 +102,39 @@ const MainPage=()=>{
         <Route path="/Bicep" render={()=>{
             return(
             <>
-            <br/>
+            <br />
            {Bedata.map(ncard1)}
            </>
             );
         }}/>
         <Route path="/timer" render={()=>{
 
-      
+      return(
+         <React.Fragment>
+          <br/>
+          <br />
+
+          <div className="container mt-3">
+          <NavLink to="/togain">Togain weight </NavLink><br/>
+          </div>
+          <div className="container">
+          <NavLink to="/toloss">To weight-loss</NavLink> <br/>
+          </div>
+        </React.Fragment>
+          
+
+      );
 
             
         }}/>
         
-           </BrowserRouter>
+      </BrowserRouter>
            
            <br/>
         <h5 className="CopyRight">© Copyright {year} by JEMISH ITALIYA</h5>
-        </>
+        </React.Fragment>
     );
     
 };
+
 export default MainPage;
