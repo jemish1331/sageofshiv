2fd6700df93ddb3611684cfeb84f29fafcd51674
jemish italiya <italiyajemish99@gmail.com>
2020-08-26 17:16:18 +0530

    deploy to git pages

--- src/MainPage.js
+++ src/MainPage.js
@@ -34,35 +34,45 @@ const MainPage=()=>{
         <React.Fragment>
     
        <BrowserRouter>
+       <div>
        <div className=" position-fixed overlap  gray  small-h-25 w-100 d-flex justify-content-center ">
         <NavLink to="/home" activeClassName="selected" className="ml-4 text-white  h4  font-weight-bolder  "><Wave  text="Profile" speed="1"  delay="7"/></NavLink>
         <NavLink to="/exercise" activeClassName="selected" className="ml-4 text-white  h4  font-weight-bolder  "><Wave  text="Exercise" speed="1" delay="7"/></NavLink>
         <NavLink to="/timer" activeClassName="selected" className="ml-4 text-white h4  font-weight-bolder "><Wave  text="Diet-plan" speed="1" delay="7"/></NavLink>
        </div>
        
-       
         <Route exact path="/" render={()=>{
             return (
+                <>
                 <div className="d-flex justify-content-center mt-5">
                 <br />
                  <br/>
-                <img src="https://i.pinimg.com/736x/67/50/8c/67508cacd7406a4cb80ee7bf489a27f9.jpg" className="ml-5 w-50 h-40 "></img>
-                 
-                <p className="display-4 text-white"> To be continued....</p>
+                <img src="https://i.pinimg.com/736x/67/50/8c/67508cacd7406a4cb80ee7bf489a27f9.jpg" ></img>
                 </div>
+                <div className="display-4 text-white d-flex justify-content-center"> To be continued....</div>
+                </>
             );
 
         }} />
+       </div>
+       
         <Route  path="/exercise" render={()=>{
             return (
                 <>
                 <br />
                 <br />
-                <div className=" gray mt-5 h-25 d-flex justify-content-center">
-                <NavLink to="/shoulder" activeClassName="selected"   className="ml-2 text-white h4  font-weight-bolder ">Shoulder-Exercise</NavLink></div> 
-                <div className=" gray mt-5 h-25 d-flex justify-content-center"> <NavLink to="/chest" activeClassName="selected"  className="ml-2 text-white h4   font-weight-bolder ">Chest-Exercise</NavLink></div>
-                <div className=" gray mt-5 h-25 d-flex justify-content-center"> <NavLink to="/leg" activeClassName="selected"  className="ml-2 text-white h4 font-weight-bolder ">Leg-Exercise</NavLink></div>
-                <div className=" gray mt-5 h-25 d-flex justify-content-center"> <NavLink to="/bicep" activeClassName="selected"  className="ml-2 text-white h4  font-weight-bolder" >Bicep-Exercise</NavLink></div>
+                <div className=" gray1 mt-5 h-25 d-flex justify-content-center">
+                <NavLink to="/shoulder" activeClassName="selected"   className="ml-2 text-primary h4  font-weight-bolder ">Shoulder-Exercise</NavLink>
+                </div> 
+                <div className=" gray1 mt-5 h-25 d-flex justify-content-center"> 
+                <NavLink to="/chest" activeClassName="selected"  className="ml-2 text-primary h4   font-weight-bolder ">Chest-Exercise</NavLink>
+                </div>
+                <div className="gray1  mt-5 h-25 d-flex justify-content-center">
+                 <NavLink to="/leg" activeClassName="selected"  className="ml-2 text-primary h4 font-weight-bolder ">Leg-Exercise</NavLink>
+                 </div>
+                <div className="gray1 mt-5 h-25 d-flex justify-content-center"> 
+                <NavLink to="/bicep" activeClassName="selected"  className="ml-2 text-primary h4  font-weight-bolder" >Bicep-Exercise</NavLink>
+                </div>
                
                    
                 <br/>
