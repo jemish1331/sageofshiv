ddc12ea3bfd7d19c0abcc4b13a55620a7a8fbdb3
jemish italiya <italiyajemish99@gmail.com>
2020-09-20 10:39:22 +0530

    Updates

---
+++ precache-manifest.f2a37f55d389e69e2772aaa8204087da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68e22b19bd46a1f35b6",
+    "url": "/sageofshiv/static/css/main.6b3b1e1f.chunk.css"
+  },
+  {
+    "revision": "a68e22b19bd46a1f35b6",
+    "url": "/sageofshiv/static/js/main.8805b53f.chunk.js"
+  },
+  {
+    "revision": "40d2f52c01beb1beb7b2",
+    "url": "/sageofshiv/static/js/runtime~main.743bba42.js"
+  },
+  {
+    "revision": "753975447f6393c6d9d5",
+    "url": "/sageofshiv/static/css/2.9f432744.chunk.css"
+  },
+  {
+    "revision": "753975447f6393c6d9d5",
+    "url": "/sageofshiv/static/js/2.aaeef5aa.chunk.js"
+  },
+  {
+    "revision": "e245c502f6fd9eb08da8e700bd7e1845",
+    "url": "/sageofshiv/index.html"
+  }
+];
